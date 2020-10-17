0f4dd224f93cfcab66dcccbcf83ef7894e30911f
Lauren Trinks <laurentrinks@gmail.com>
2020-10-17 16:11:31 -0400

    Stack Overflow Search Results
    a class called Results with method "getResults" that takes a space-separated list of up to 5 tags and returns a JSON of the results

---
+++ StackExchangeAPI.py
@@ -0,0 +1,66 @@
+import requests
+import json
+import sys
+
+
+class Results:
+
+    def getResults(tags):
+
+        # add a space to the end of tags to prevent crashing and initialize individual tags
+        tags = tags + " "
+        tag1 = " "
+        tag2 = " "
+        tag3 = " "
+        tag4 = " "
+        tag5 = " "
+
+        # isolate each tag by spaces
+        if len(tags) > 0:
+            tag1 = tags[0:tags.index(" ")]
+            tags = tags[tags.index(" ") + 1:]
+
+        if len(tags) > 0:
+            tag2 = tags[0:tags.index(" ")]
+            tags = tags[tags.index(" ") + 1:]
+
+        if len(tags) > 0:
+            tag3 = tags[0:tags.index(" ")]
+            tags = tags[tags.index(" ") + 1:]
+
+        if len(tags) > 0:
+            tag4 = tags[0:tags.index(" ")]
+            tags = tags[tags.index(" ") + 1:]
+
+        if len(tags) > 0:
+            tag5 = tags[0:tags.index(" ")]
+            tags = tags[tags.index(" ") + 1:]
+
+        # create the url from the tags that the API will collect
+        tagSplit = "%3B%20"
+        buildURL = ("https://api.stackexchange.com/2.2/tags/" + tag1 + tagSplit + tag2 + tagSplit + tag3 + tagSplit +
+                    tag4 + tagSplit + tag5 + "/faq?site=stackoverflow")
+
+        # url for testing, no longer needed
+        testURL = "https://api.stackexchange.com/2.2/tags/java%3B%20exception%3B%20FileNotFoundException/" \
+                  "faq?site=stackoverflow"
+
+        # get the response from the API and save it as a JSON object
+        url = buildURL
+        response = requests.get(url)
+        responseJSON = response.json()
+
+        # For testing, print all of the results' link and title
+        """
+        for i in range(len(responseJSON['items'])):
+           print((responseJSON['items'][i]['link']))
+           print((responseJSON['items'][i]['title']))
+        """
+
+        # return the JSON object
+        return responseJSON
+
+
+
+
+
