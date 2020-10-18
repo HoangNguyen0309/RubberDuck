6b3998cc97e9e4165364c50ecb5583e42d01a6ad
Lauren Trinks <laurentrinks@gmail.com>
2020-10-17 20:53:57 -0400

    changed input syntax
    keywords can now have spaces, and are separated by commas by the user

--- StackExchangeAPI.py
+++ StackExchangeAPI.py
@@ -7,8 +7,11 @@ class Results:
 
     def getResults(tags):
 
-        # add a space to the end of tags to prevent crashing and initialize individual tags
-        tags = tags + " "
+        # replace the spaces with underlines
+        tags = Results.getUsableTags(tags)
+
+        # add a comma to the end of tags to prevent crashing and initialize individual tags
+        tags = tags + ","
         tag1 = " "
         tag2 = " "
         tag3 = " "
@@ -16,25 +19,25 @@ class Results:
         tag5 = " "
 
         # isolate each tag by spaces
-        if len(tags) > 0:
-            tag1 = tags[0:tags.index(" ")]
-            tags = tags[tags.index(" ") + 1:]
+        if (len(tags) > 0) & ("," in tags):
+            tag1 = tags[0:tags.index(",")]
+            tags = tags[tags.index(",") + 1:]
 
-        if len(tags) > 0:
-            tag2 = tags[0:tags.index(" ")]
-            tags = tags[tags.index(" ") + 1:]
+        if (len(tags) > 0) & ("," in tags):
+            tag2 = tags[0:tags.index(",")]
+            tags = tags[tags.index(",") + 1:]
 
-        if len(tags) > 0:
-            tag3 = tags[0:tags.index(" ")]
-            tags = tags[tags.index(" ") + 1:]
+        if (len(tags) > 0) & ("," in tags):
+            tag3 = tags[0:tags.index(",")]
+            tags = tags[tags.index(",") + 1:]
 
-        if len(tags) > 0:
-            tag4 = tags[0:tags.index(" ")]
-            tags = tags[tags.index(" ") + 1:]
+        if (len(tags) > 0) & ("," in tags):
+            tag4 = tags[0:tags.index(",")]
+            tags = tags[tags.index(",") + 1:]
 
-        if len(tags) > 0:
-            tag5 = tags[0:tags.index(" ")]
-            tags = tags[tags.index(" ") + 1:]
+        if (len(tags) > 0) & ("," in tags):
+            tag5 = tags[0:tags.index(",")]
+            tags = tags[tags.index(",") + 1:]
 
         # create the url from the tags that the API will collect
         tagSplit = "%3B%20"
@@ -53,14 +56,20 @@ class Results:
         # For testing, print all of the results' link and title
         """
         for i in range(len(responseJSON['items'])):
-           print((responseJSON['items'][i]['link']))
            print((responseJSON['items'][i]['title']))
+           print((responseJSON['items'][i]['link']))
         """
 
         # return the JSON object
         return responseJSON
 
-
-
-
-
+    # replaces the spaces with underlines
+    def getUsableTags(tags):
+        workingTags = ""
+        originalTag = tags
+        for i in range(len(tags)):
+              if originalTag[i] == " ":
+                  workingTags = workingTags + "_"
+              else:
+                  workingTags = workingTags + originalTag[i]
+        return workingTags
