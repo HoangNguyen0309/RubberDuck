81062ac4f68f479f186500199c46bab35e57c3b5
HoangNguyen0309 <70375148+HoangNguyen0309@users.noreply.github.com>
2020-10-18 10:50:46 +0700

    Added the HTML, CSS and JS frames

---
+++ duck.js
@@ -0,0 +1,8 @@
+window.addEventListener('keypress', (e) => {
+    // Press enter button on keyboard
+if (e.keyCode === 13) {
+    document.getElementById("change").href = "newRubber.css";
+            
+}
+})
+
